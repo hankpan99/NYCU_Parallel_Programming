5a85b2d79e0d14a68aa08c34fed437a052e52ee1
hankpan99 <yeyehank@gmail.com>
2023-01-03 21:35:51 -0500

    [HW6] [v1]: test hostFE.c, kernel.cl

--- HW6/src/hostFE.c
+++ HW6/src/hostFE.c
@@ -8,6 +8,52 @@ void hostFE(int filterWidth, float *filter, int imageHeight, int imageWidth,
             cl_context *context, cl_program *program)
 {
     cl_int status;
-    int filterSize = filterWidth * filterWidth;
+    int filterSize = filterWidth * filterWidth * sizeof(float);
+    int imageSize = imageHeight * imageWidth * sizeof(float);
 
+    // create command queue
+    cl_command_queue myqueue;
+    myqueue = clCreateCommandQueue(*context, *device, 0, &status);
+
+    // create buffers on device
+    cl_mem device_in_img = clCreateBuffer(*context, CL_MEM_READ_ONLY, imageSize, NULL, &status);
+    cl_mem device_out_img = clCreateBuffer(*context, CL_MEM_WRITE_ONLY, imageSize, NULL, &status);
+    cl_mem device_filter = clCreateBuffer(*context, CL_MEM_READ_ONLY, filterSize, NULL, &status);
+
+    // transfer data from host to device
+    status = clEnqueueWriteBuffer(myqueue, device_in_img, CL_TRUE, 0, imageSize, (void*) inputImage, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer");
+    status = clEnqueueWriteBuffer(myqueue, device_filter, CL_TRUE, 0, filterSize, (void*) filter, 0, NULL, NULL);
+    CHECK(status, "clEnqueueWriteBuffer");
+
+    // create kernel function
+    cl_kernel mykernel = clCreateKernel(*program, "convolution", &status);
+    CHECK(status, "clCreateKernel");
+
+    // set arguments
+    clSetKernelArg(mykernel, 0, sizeof(cl_int), (void*) &filterWidth);
+    clSetKernelArg(mykernel, 1, sizeof(cl_mem), (void*) &device_filter);
+    clSetKernelArg(mykernel, 2, sizeof(cl_int), (void*) &imageHeight);
+    clSetKernelArg(mykernel, 3, sizeof(cl_int), (void*) &imageWidth);
+    clSetKernelArg(mykernel, 4, sizeof(cl_mem), (void*) &device_in_img);
+    clSetKernelArg(mykernel, 5, sizeof(cl_mem), (void*) &device_out_img);
+
+    // set local and global workgroup sizes
+    size_t localws[2] = {20, 20};
+    size_t globalws[2] = {imageWidth, imageHeight};
+
+    // execute kernel function
+    status = clEnqueueNDRangeKernel(myqueue, mykernel, 2, 0, globalws, localws, 0, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
+
+    // copy reuslts from device back to host
+    status = clEnqueueReadBuffer(myqueue, device_out_img, CL_TRUE, 0, imageSize, (void*) outputImage, NULL, NULL, NULL);
+    CHECK(status, "clEnqueueNDRangeKernel");
+
+    // release
+    status = clReleaseCommandQueue(myqueue);
+    status = clReleaseMemObject(device_in_img);
+    status = clReleaseMemObject(device_out_img);
+    status = clReleaseMemObject(device_filter);
+    status = clReleaseKernel(mykernel);
 }
--- HW6/src/kernel.cl
+++ HW6/src/kernel.cl
@@ -1,4 +1,19 @@
-__kernel void convolution() 
+__kernel void convolution(int filterWidth, __constant float *filter, int imageHeight, int imageWidth,
+                          __global float *inputImage, __global float *outputImage)
 {
-   
+    int halffilterSize = filterWidth / 2;
+    float sum = 0;
+
+    // get thread index within index space
+    const int ix = get_global_id(0), iy = get_global_id(1);
+
+    for(int k = -halffilterSize; k <= halffilterSize; k++){
+        for(int l = -halffilterSize; l <= halffilterSize; l++){
+            if((iy + k >= 0) && (iy + k < imageHeight) && (ix + l >= 0) && (ix + l < imageWidth)){
+                sum += (inputImage[(iy + k) * imageWidth + ix + l] * filter[(k + halffilterSize) * filterWidth + l + halffilterSize]);
+            }
+        }
+    }
+
+    outputImage[iy * imageWidth + ix] = sum;
 }
